3e7d28c42bee7c26c8f648bf316fcde6bd614ac8
Taha Yasin Muçak <tahayasinmucak@Tahas-MacBook.fritz.box>
2025-05-16 18:11:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/*let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}, 10000); // 10 seconds
+*/
